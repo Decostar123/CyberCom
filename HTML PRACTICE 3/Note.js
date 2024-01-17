571b91b3c8681d860a1ebe081d20b1606fbeabdc
Decostar123 <decostarsharma113@gmail.com>
2024-01-17 15:42:12 +0530

    intial commit

---
+++ HTML PRACTICE 3/Note.js
@@ -0,0 +1,5 @@
+// 1) Applying rowspan to all the column do not make sense, it means
+//  increase the space withn , so one td will hve less rowspan 
+// 2) rowspan and column span not include the current row or column 
+// it means the extar one 
+// 3) by default no row and columns 
